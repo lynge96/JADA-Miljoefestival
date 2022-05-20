9e069199911b6fe8dbfea2a072da88da487db8bc
Jacob Kaae <jacobkaae0511@gmail.com>
2022-05-20 14:10:26 +0200

    fefe

---
+++ MiljøFestival/MiljøFestival/Server/Controllers/KompetenceController.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MiljøFestival.Shared.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MiljøFestival.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KompetenceController : ControllerBase
+    {
+        private string connString = "User ID=systemadmin;Password=Festival987;Host=jadafestival-db.postgres.database.azure.com;Port=5432;Database=postgres;";
+
+        // Hent alle kompetencer
+        [HttpGet("all")]
+        public async Task<IEnumerable<string>> HentAlle()
+        {
+            var sql = "SELECT kompetence FROM kompetence;";
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connString))
+                {
+                    var kompetenceListe = await connection.QueryAsync<string>(sql);
+
+                    return kompetenceListe;
+                }
+            }
+            catch (System.Exception)
+            {
+                return new List<string>();
+            }
+        }
+
+
+    }
+}
