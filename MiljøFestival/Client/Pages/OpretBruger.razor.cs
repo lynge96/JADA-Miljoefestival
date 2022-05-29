189722c5d8e9374a393429d63d0a54064700c6c1
Anders <90785699+lynge96@users.noreply.github.com>
2022-05-29 21:53:37 +0200

    Masser af finpudsning + validation

---
+++ MiljøFestival/Client/Pages/OpretBruger.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using MiljøFestival.Shared.Models;
+
+namespace MiljøFestival.Client.Pages
+{
+    public partial class OpretBruger
+    {
+        private Bruger BrugerModel = new Bruger();
+        private EditContext EditContext;
+
+        protected override void OnInitialized()
+        {
+            EditContext = new EditContext(BrugerModel);
+        }
+    }
+}
